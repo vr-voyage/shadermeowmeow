05b042dd1ae67483e3b3873ba1a105df57a08f29
Voyage <voyage@miouyouyou.fr>
2023-01-12 19:00:22 +0100

    The first test is working !
    But there's still no actual framework yet.
    Soon...
    
    Signed-off-by: Voyage <voyage@miouyouyou.fr>

--- shadermotion_swingtwist_tests.gd
+++ shadermotion_swingtwist_tests.gd
@@ -31,7 +31,7 @@ func _parse_json_object(json_object:Dictionary):
 
 	var object_type:String = json_object["@type"] as String
 	if object_type == null:
-		printerr("Invalid object type !")
+		printerr("Invalid object type (@type is not a String !)")
 		return invalid_value
 
 	if not json_object_parsers.has(object_type):
@@ -110,43 +110,75 @@ func _is_method_analysis(parsed_json:Dictionary) -> bool:
 			return false
 	return parsed_json["@type"] == "@MethodAnalysis" and parsed_json["version"] == 1
 
+func _method_analysis_io_content(io_object:Dictionary):
+	var content:Dictionary = io_object["content"]
+
+	match(typeof(content)):
+		TYPE_FLOAT, TYPE_STRING, TYPE_NIL:
+			return content
+		TYPE_DICTIONARY:
+			return _parse_json_object(content)
+		_:
+			printerr(
+				"Don't know how to deal with content of type : %d (%s)"
+				% [typeof(content), str(content)])
+			return null
+
+func _method_analysis_io_get(
+	inputs_outputs:Dictionary,
+	value_name:String,
+	io_type:String
+):
+	var returned_value = null
+
+	if not inputs_outputs.has(io_type):
+		printerr("Invalid MethodInputOutput object !")
+		return returned_value
+
+	var io_category = inputs_outputs[io_type] as Array
+	if io_category == null:
+		printerr(
+			"This MethodInputOutput '%s' field is invalid"
+			% [io_type])
+		return returned_value
+
+	if io_category.is_empty():
+		return returned_value
+
+	for value in io_category:
+		var value_description:Dictionary = value as Dictionary
+		if value_description == null:
+			printerr(
+				"Expected a Dictionary in '%s'. Got : %s"
+				% [io_type, str(value)])
+			continue
+				
+		if not value_description.has_all(["name", "content"]):
+			printerr(
+				"Invalid %s data : %s"
+				% [io_type, str(value_description)])
+			continue
+
+		if value_description["name"] != value_name:
+			continue
+
+		returned_value = _method_analysis_io_content(value_description)
+		break
+
+	return returned_value
+
+
 func _method_analysis_get_input(
 	inputs_outputs:Dictionary,
 	input_name:String
-) -> Dictionary:
+):
+	return _method_analysis_io_get(inputs_outputs, input_name, "input")
 
-	var invalid_value:Dictionary = {}
-
-	if not inputs_outputs.has("input"):
-		printerr("Invalid MethodInputOutput object !")
-		return invalid_value
-
-	var inputs = inputs_outputs["input"] as Array
-	if inputs == null:
-		printerr("This MethodInputOutput 'input' field is invalid")
-		return invalid_value
-
-	if inputs.is_empty():
-		return invalid_value
-
-	for input_value in inputs:
-		var input_description:Dictionary = input_value as Dictionary
-		if input_description == null:
-			printerr(
-				"Expected a Dictionary in 'input'. Got : %s"
-				% [str(input_value)])
-			continue
-				
-		if not input_description.has_all(["name", "content"]):
-			printerr("Invalid input data : %s" % [str(input_description)])
-			continue
-
-		if input_description.name != input_name:
-			continue
-
-		return input_description["content"]
-
-	return invalid_value
+func _method_analysis_get_output(
+	inputs_outputs:Dictionary,
+	output_name
+):
+	return _method_analysis_io_get(inputs_outputs, output_name, "output")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -166,12 +198,18 @@ func _ready():
 		printerr("%s is not a MethodAnalysis JSON file." % results_json_filepath)
 		return
 
-	var data = _parse_json_object(_method_analysis_get_input(
+	var scaled_swing_twist:Vector3 = _method_analysis_get_input(
 		parsed_json["execution"][0],
-		"scaledSwingTwist"))
-	printerr(data)
+		"scaledSwingTwist") as Vector3
+	var expected_rotation:Quaternion = _method_analysis_get_output(
+		parsed_json["execution"][0],
+		"rotation") as Quaternion
+	printerr(scaled_swing_twist)
 
-	print(ShaderMotionHelpers.swing_twist(data))
+	var our_rotation:Quaternion = ShaderMotionHelpers.swing_twist(scaled_swing_twist)
+	printerr(expected_rotation)
+	printerr(our_rotation)
+	printerr(expected_rotation.is_equal_approx(our_rotation))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
