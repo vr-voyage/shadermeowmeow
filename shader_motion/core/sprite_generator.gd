a7ba28b1750ed89c5fedf05d528a0bdcf0a49df7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-01-28 22:05:51 -0800

    Duplicate the texture.

--- shader_motion/core/sprite_generator.gd
+++ shader_motion/core/sprite_generator.gd
@@ -9,7 +9,7 @@ func _enter_tree():
 
 func _process(_delta):
 	var texture : Texture2D = get_video_texture()
-	ShaderMotionHelpers.get_shader_motion_tiles_from_texture(texture, frames, stream_position)
+	ShaderMotionHelpers.get_shader_motion_tiles_from_texture(texture.duplicate(), frames, stream_position)
 	if stream_position > 0.5:
 		ResourceSaver.save(frames, "res://shader_motion/frames/result_frames.res", ResourceSaver.FLAG_BUNDLE_RESOURCES)
 		get_tree().quit()
