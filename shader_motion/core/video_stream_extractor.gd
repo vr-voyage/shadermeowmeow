38c909db15a19f7bde37f185a6231eb62ebbde56
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-01-31 19:16:58 -0800

    Trim the code.

--- shader_motion/core/video_stream_extractor.gd
+++
@@ -1,8 +0,0 @@
-extends VideoStreamPlayer
-
-func _ready():
-	Engine.time_scale = 1.0 / 10
-
-func _process(delta):
-	print("Tick and Dimensions in texture: %s %s" % [stream_position, get_video_texture().get_size()])
-	ResourceSaver.save(get_video_texture(), "res://shader_motion/shadermotion_the_devouring_frames/frame_%f.png" % stream_position)
